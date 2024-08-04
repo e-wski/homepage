32c62fdccd5babfcb54f53925ed3febd2e1dfc9c
Łukasz Ostaszewski <kontakt.ostaszewski@gmail.com>
2024-08-04 10:03:41 +0200

    Innitial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('Litr żołądkowej to obowiąkowe!')
+
+let hacked = document.querySelector('.button')
+let action = document.querySelector('.js-action')
+let actionName = document.querySelector('.js-actionName')
+
+hacked.addEventListener('click', () => {
+	action.classList.toggle('hide')
+	actionName.innerText = action.classList.contains('hide') ? 'Przywróć' : 'Hackuj'
+})
